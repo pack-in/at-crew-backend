45bd3589ca5e1167f46969388fafc0e3ada40ea4
danhandev <danhandev@gmail.com>
2026-06-01 15:20:04 +0900

    init: 앳크루 백엔드 프로젝트 초기 설정

---
+++ src/main/java/com/atcrew/AtCrewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.atcrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtCrewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtCrewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atcrew/AtCrewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtCrewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
